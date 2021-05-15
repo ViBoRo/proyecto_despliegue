8e212b3721ec06b17b4176444578bc62c38cd406
ViBoRo <viborro@epsg.upv.es>
2021-05-15 21:21:13 +0200

    coger solo campos que nos interesan

--- recoger_datos.sh
+++ recoger_datos.sh
@@ -3,11 +3,12 @@ fecha=$(date | awk '{print $2" "$3" "$6}'| tr ' ' '_')
 hora=$(date | awk '{print $4}' | awk -F":" '{print $1}')
 
 #guardar datos de las acciones en un fichero local mes_dia_anyo.txt
-datos=$(python scraper.py)
-echo "$datos" >> "$fecha.txt"
+datos=$(python scraper.py | awk -F"," '{print $1","$2","$6","$7","$10}')
+
+echo "$datos" >> "$fecha.csv"
 
 #a las 18:30 guardar el fichero local en hdfs i luego lo borro
 if [ $hora == "18" ]; then
-echo $(hdfs dfs -put "$fecha.txt" ibex)
+echo $(hdfs dfs -put "$fecha.csv" ibex)
 echo $(rm "$fecha.txt")
 fi

07fdafd29c6d246f1c9167b5ef7e0d24225aa293
ViBoRo <viborro@epsg.upv.es>
2021-05-29 20:41:34 +0200

    listado_semanal_mensual

--- 1_listadosemanal.py
+++ 1_listadosemanal.py
@@ -1,39 +1,43 @@
 from mrjob.job import MRJob
-from mrjob.step import MRStep
+#from mrjob.step import MRStep
 
 
-class Listado(MRJob):
+class Semanal(MRJob):
     
 
     def mapper(self, _, line):
         
         data = line.split(",")
-        if data[8] == "Gold" and int(data[0]) >= 1950:
-            yield(data[5], 1)
+        nombre = data[0]
+        ultimo = data[1]
+        maximo = data[2]
+        minimo = data[3]
+        yield(nombre, (maximo, minimo, ultimo))
                 
 
-    def reducer(self, country, values):
-        yield("", (country, sum(values)))
+    def reducer(self, nombre, values):
+        primero = None
+        maximo = 0
+        primer_minimo = False
+        minimo = 0
+        final = 0
+        for maximos, minimos, ultimos in values:
 
-    def reducer2(self, clavecomun, tupla_country_medals):
-        max_country = ""
-        max_medals = 0
-        
-        for country, medals in tupla_country_medals:
-            if(int(medals)>max_medals):
-                max_medals = medals
-                max_country = country
+            if primero == None:
+                primero = maximos
+            
+            if float(maximos) > float(maximo):
+                maximo = maximos
 
-        yield(max_country, max_medals)
-
-    def steps(self):
-        return [
-            MRStep(mapper = self.mapper,
-                   reducer = self.reducer),
-            MRStep(reducer = self.reducer2)
-        ]
-        
+            if primer_minimo == False:
+                minimo = minimos
+                primer_minimo == True
+            if float(minimos) < float(minimo):
+                minimo = minimos
+            
+            final = ultimos
+                
+        yield(nombre, (primero, final, minimo, maximo))
     
-      
 if __name__ == '__main__':
-  Listado.run()
+  Semanal.run()
--- recoger_datos.sh
+++ recoger_datos.sh
@@ -1,14 +1,19 @@
 #guardar fecha y hora
-fecha=$(date | awk '{print $2" "$3" "$6}'| tr ' ' '_')
+fecha=$(date +"%F")
+num_semana=$(date +"%W")
+mes=$(date +"%m")
+dia=$(date +"%d")
+any=$(date +"%Y")
 hora=$(date | awk '{print $4}' | awk -F":" '{print $1}')
 
-#guardar datos de las acciones en un fichero local mes_dia_anyo.txt
+#guardar datos de las acciones en un fichero local anyo_mes_dia.txt
 datos=$(python scraper.py | awk -F"," '{print $1","$2","$6","$7","$10}')
 
 echo "$datos" >> "$fecha.csv"
 
 #a las 18:30 guardar el fichero local en hdfs i luego lo borro
 if [ $hora == "18" ]; then
-echo $(hdfs dfs -put "$fecha.csv" ibex)
-echo $(rm "$fecha.txt")
+echo $(hdfs dfs -mkdir -p ibex/$any/$mes/$num_semana/$dia)
+echo $(hdfs dfs -put "$fecha.csv" /ibex/$any/$mes/$num_semana/$dia)
+echo $(rm "$fecha.csv")
 fi
